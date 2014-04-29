c91615db31476c12d90e0e6b2d23906b4edf4371
gitluochao <luochao@jd.com>
2014-04-29 16:41:33 +0800

    git

--- go-server/out/production/go-server/go-server.go
+++ go-server/out/production/go-server/go-server.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
 	"handler"
+	"net/http"
+	"os/exec"
+	"reflect"
+	"util"
 )
 
-func main() {
-	fmt.Println(http.Dir("webapp"))
-	http.Handle("/css/",http.FileServer(http.Dir("webapp")))
-	http.Handle("/js/",http.FileServer(http.Dir("webapp")))
-	http.HandleFunc("/login/",handler.GetLoginHanlder())
-	http.HandleFunc("/admin/",handler.GetAdminHanlder())
-	http.HandleFunc("/ajax/",handler.GetAjaxHanlder())
-	http.HandleFunc("/",handler.GetNotFundHandler())
-	http.ListenAndServe(":8090",nil)
+var logger = &util.LoggerHelp{}
 
+func main() {
+	cmd := exec.Command("pwd", []string{})
+	cmd.Start()
+	fmt.Println(http.Dir("webapp"))
+	logger.Info("reflect.Value")
+	fmt.Println(reflect.ValueOf(logger).MethodByName("Info").Call([]reflect.Value{reflect.ValueOf("message")}))
+	http.Handle("/css/", http.FileServer(http.Dir("webapp")))
+	http.Handle("/js/", http.FileServer(http.Dir("webapp")))
+	http.HandleFunc("/login/", handler.GetLoginHanlder())
+	http.HandleFunc("/admin/", handler.GetAdminHanlder())
+	http.HandleFunc("/ajax/", handler.GetAjaxHanlder())
+	http.HandleFunc("/", handler.GetNotFundHandler())
+	http.ListenAndServe(":9090", nil)
 }
--- go-server/src/util/Logger.go
+++ go-server/src/util/Logger.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -8,33 +9,35 @@ import (
 
 var filename = "info.log"
 var logger = &log.Logger{}
+
 type LoggerHelp struct {
 }
-func init(){
-	out,err := os.Open(filename)
+
+func initLog() {
+	out, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0)
+	fmt.Println(filename)
 	if err != nil {
 
 	}
-	logger = log.New(out,time.Now().String(),log.Ldate|log.Ltime|log.Llongfile)
+	logger = log.New(out, time.Now().String(), log.Ldate|log.Ltime|log.Llongfile)
 }
 
-func (loggerHelp *LoggerHelp) Info(message string){
-	 logger.Println(message)
-}
-
-func (loggerHelp *LoggerHelp) Error(message string){
+func (loggerHelp *LoggerHelp) Info(message string) {
+	fmt.Println("get log info ")
+	initLog()
 	logger.Println(message)
 }
 
-func (loggerHelp *LoggerHelp) Debug(message string){
-	logger.Println(message)
-}
-func (loggerHelp *LoggerHelp) Warn(message string){
-	logger.Println(message)
-}
-func (loggerHelp *LoggerHelp) Fatal(message string){
+func (loggerHelp *LoggerHelp) Error(message string) {
 	logger.Println(message)
 }
 
-
-
+func (loggerHelp *LoggerHelp) Debug(message string) {
+	logger.Println(message)
+}
+func (loggerHelp *LoggerHelp) Warn(message string) {
+	logger.Println(message)
+}
+func (loggerHelp *LoggerHelp) Fatal(message string) {
+	logger.Println(message)
+}
